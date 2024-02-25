293033a2393265b821381f2aacce0b7155f89a01
zack-horton <zhorton007@gmail.com>
2024-02-24 21:37:33 -0500

    added lyft xgboost

--- models/forest.py
+++ models/forest.py
@@ -128,9 +128,9 @@ def fit_DT(train_x, train_y, test_x, test_y, company, save_model=False):
 def fit_forest(train_x, train_y, test_x, test_y, company, save_model=False):
     train_y = train_y.ravel()
     test_y = test_y.ravel()
-    fit_DT(train_x, train_y, test_x, test_y, company, save_model)
-    # fit_RF(train_x, train_y, test_x, test_y, company, save_model)
-    # fit_XGB(train_x, train_y, test_x, test_y, company, save_model)
+    # fit_DT(train_x, train_y, test_x, test_y, company, save_model)
+    fit_XGB(train_x, train_y, test_x, test_y, company, save_model)
+    fit_RF(train_x, train_y, test_x, test_y, company, save_model)
 
 lyft_train_x, lyft_train_y, lyft_test_x, lyft_test_y = get_data('lyft')
 print("lyft train:", len(lyft_train_x), "lyft test:", len(lyft_test_x))
